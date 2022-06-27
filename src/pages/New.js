208c9a2327c22559d35684e0944e3596f7ead642
alsth712 <alsth712@naver.com>
2022-06-28 00:32:16 +0900

    🔨 New 구현

---
+++ src/pages/New.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import DiaryEditor from "../components/DiaryEditor";
+
+const New = () => {
+  // page별 title 변경
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장 - 새 일기`;
+  }, []);
+
+  return (
+    <div>
+      <DiaryEditor />
+    </div>
+  );
+};
+
+export default New;
